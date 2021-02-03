7810208a912ad639a31da8ed29faa3defdfcb1cd
Bailey Clemons <baileythevampireslayer@gmail.com>
2021-02-02 21:49:04 -0800

    deploying

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -19,14 +19,22 @@ module.exports = function (app) {
 
 
   app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}, {})
+    db.Workout.aggregate([{
+      $addFields: {
+        totalDuration:
+          { $sum: "$exercises.duration" }
+      }
+    }])
+      .sort({ _id: -1 })
+      .limit(7)
       .then(dbWorkout => {
         res.json(dbWorkout);
       })
       .catch(err => {
         res.json(err);
-      }); 
+      });
   });
+
 
   app.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
